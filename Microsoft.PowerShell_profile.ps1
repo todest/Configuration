f13c44125411b6ce1d9ecbec53809d99daa17f0e
todest <33983947+todest@users.noreply.github.com>
2022-02-19 12:54:16 +0800

    Add PS Proxy

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,32 @@
+$regPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+function Clear-Proxy
+{
+    Set-ItemProperty -Path $regPath -Name ProxyEnable -Value 0
+    Set-ItemProperty -Path $regPath -Name ProxyServer -Value ''
+    Set-ItemProperty -Path $regPath -Name ProxyOverride -Value ''
+
+    [Environment]::SetEnvironmentVariable('http_proxy', $null, 'User')
+    [Environment]::SetEnvironmentVariable('https_proxy', $null, 'User')
+}
+
+function Set-Proxy
+{
+    $proxy = 'http://127.0.0.1:10809'
+
+    Set-ItemProperty -Path $regPath -Name ProxyEnable -Value 1
+    Set-ItemProperty -Path $regPath -Name ProxyServer -Value $proxy
+    Set-ItemProperty -Path $regPath -Name ProxyOverride -Value '<local>'
+
+    [Environment]::SetEnvironmentVariable('http_proxy', $proxy)
+    [Environment]::SetEnvironmentVariable('https_proxy', $proxy)
+}
+Set-Proxy
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module Get-ChildItemColor
+Set-PoshPrompt -Theme powerlevel10k_rainbow
